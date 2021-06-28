700f8b1b3a441f96b86d3b85c4c346f41a3ad8a1
Arsh_026 <preetk172@gmail.com>
2021-06-28 17:28:25 -0400

    First Commit

---
+++ src/test/java/testCases/LoginTestPage.java
@@ -0,0 +1,59 @@
+package testCases;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import base.TestBase;
+import page.DatabasePage;
+import page.LoginPage;
+
+public class LoginTestPage extends TestBase {
+	LoginPage loginPageObj;
+	SoftAssert softAssert = new SoftAssert();
+	
+	@BeforeMethod 
+	public void setUp() {
+		initializeDriver();
+		loginPageObj = PageFactory.initElements(driver, LoginPage.class);
+	}
+	
+	@Test
+	public void LoginTest() throws InterruptedException, ClassNotFoundException, SQLException, IOException {
+		loginPageObj.enterUserName(DatabasePage.getData("username"));
+		loginPageObj.enterPassword(DatabasePage.getData("password"));
+		loginPageObj.clickOnSignInButton();
+		Thread.sleep(3000);
+		
+		takeScreenshotAtEndOfTest(driver);
+	}
+	
+	//@Test
+//	public void loginPageTitleTest() throws ClassNotFoundException, SQLException, InterruptedException {
+//		loginPageObj.enterUserName("demo@techfios.com");
+//		Thread.sleep(2000);
+//		loginPageObj.enterPassword("abc123");
+//		Thread.sleep(2000); 
+//		loginPageObj.clickOnSignInButton();
+//		
+//		
+//		String expectedTitle = "Dashboard- iBilling";
+//		String actualTitle = loginPageObj.getPageTitle();
+//		Assert.assertEquals(expectedTitle , actualTitle);
+	//	softAssert.assertEquals(expectedTitle , actualTitle);
+//		
+//	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+
+}
